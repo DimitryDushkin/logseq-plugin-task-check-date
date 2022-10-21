a3458c2ee6093b97896c7360150c5666ceb87f18
Dmitrii Dushkin <dndushkin@fb.com>
2022-10-21 09:54:38 +0100

    remove one more console call

--- index.ts
+++ index.ts
@@ -4,8 +4,6 @@ import { getDateForPage } from "logseq-dateutils";
 const TASK_MARKERS = new Set(['DONE', 'NOW', "LATER"]);
 
 function main() {
-  console.info(`logseq-plugin-task-check-date: MAIN`);
-
   logseq.DB.onChanged(async (e) => {
     const taskBlock  = e.blocks.find(block => TASK_MARKERS.has(block.marker));
     if (!taskBlock) {
